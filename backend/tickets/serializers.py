4777ca7ca5ef2a151ba67ccc79bec3c27937bf1b
dewisbrown <luismoreno2103@gmail.com>
2023-12-15 16:09:19 -0600

    added User serializer and linked tickets to user

--- backend/tickets/serializers.py
+++ backend/tickets/serializers.py
@@ -3,15 +3,31 @@ Allows complex data to be converted to Python datatypes,
 which can then be rendered into JSON, XML, etc.
 """
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
 from .models import Ticket
 
-class TicketSerializer(serializers.ModelSerializer):
+
+class TicketSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Ticket
         fields = [
-            'author',
-            'pub_date',
+            'url',
+            'id',
+            'owner',
             'content',
             'resolved',
+        ]
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    tickets = serializers.HyperlinkedRelatedField(many=True, view_name='ticket-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'url',
+            'id',
+            'username',
+            'tickets',
         ]
