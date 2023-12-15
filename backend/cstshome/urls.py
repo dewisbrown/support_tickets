0b818cd1247a408c9f8dad324a80cf13b112dac9
dewisbrown <luismoreno2103@gmail.com>
2023-12-15 16:07:02 -0600

    added api-auth urls from rest_framework

--- backend/cstshome/urls.py
+++ backend/cstshome/urls.py
@@ -3,11 +3,13 @@ Defining urls for the entire project. API and tickets urls
 are further defined in their respective urls.py files.
 """
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api/tickets/', include('tickets.urls')),
+    path('api/', include('tickets.urls')),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
