94c437c9e9f555b65dd69ff9366030136894fb2c
dewisbrown <luismoreno2103@gmail.com>
2024-01-04 11:23:40 -0600

    updated get_queryset function to return all tickets for staff

--- backend/tickets/mixins.py
+++ backend/tickets/mixins.py
@@ -1,10 +1,28 @@
 from rest_framework import permissions
 
-from .permissions import IsStaffEditorPermission
+from .permissions import IsStaffEditorPermission, IsOwnerOrReadOnly
 
 
 class StaffEditorPermissionMixin():
     permission_classes = [
         permissions.IsAdminUser,
         IsStaffEditorPermission,
+        IsOwnerOrReadOnly,
     ]
+
+
+class UserQuerySetMixin():
+    """
+    Provides custom get_queryset function to filter by user.
+    """
+    owner_field = 'owner'
+    def get_queryset(self, *args, **kwargs):
+        # staff can view all tickets
+        if self.request.user.is_staff:
+            return super().get_queryset(*args, **kwargs)
+        
+        # filter by request user if not staff
+        lookup_data = {}
+        lookup_data[self.owner_field] = self.request.user
+        qs = super().get_queryset(*args, **kwargs)
+        return qs.filter(**lookup_data)
