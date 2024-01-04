2adb093122afe4a1238204c6a6c538bd4cebec71
dewisbrown <luismoreno2103@gmail.com>
2024-01-04 07:42:06 -0600

    included auth endpoint

--- backend/cstshome/urls.py
+++ backend/cstshome/urls.py
@@ -4,10 +4,13 @@ are further defined in their respective urls.py files.
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tickets.urls')),
+    path('api/auth/', obtain_auth_token),
 ]
 
 urlpatterns += [
