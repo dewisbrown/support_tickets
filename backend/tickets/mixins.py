bd8b4e4c959c0761b51769cfb4984a4addc951a7
dewisbrown <luismoreno2103@gmail.com>
2024-01-04 12:08:31 -0600

    moved get_qs to views.py

--- backend/tickets/mixins.py
+++ backend/tickets/mixins.py
@@ -8,20 +8,3 @@ class StaffEditorPermissionMixin():
         permissions.IsAdminUser,
         IsStaffEditorPermission,
     ]
-
-
-class UserQuerySetMixin():
-    """
-    Provides custom get_queryset function to filter by user.
-    """
-    owner_field = 'owner'
-    def get_queryset(self, *args, **kwargs):
-        # staff can view all tickets
-        if self.request.user.is_staff:
-            return super().get_queryset(*args, **kwargs)
-        
-        # filter by request user if not staff
-        lookup_data = {}
-        lookup_data[self.owner_field] = self.request.user
-        qs = super().get_queryset(*args, **kwargs)
-        return qs.filter(**lookup_data)
